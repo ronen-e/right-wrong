990ad95d363b44d42b0cacdea470e29b220828e9
Ronen E <alter.ronen@gmail.com>
2017-07-01 19:06:15 +0300

    [FIX] show footer
    - Refactor

--- index.js
+++ index.js
@@ -2,9 +2,10 @@ function main() {
   console.clear();
 
   // create and init slider
-  var manager = new MySlideManager().start();
-  initVue({manager, answers});
-  PubSub.subscribe(ANSWER, (type, payload) => fx.updateResult(payload));
+  var manager = new MySlideManager();
+  
+  initVue({ manager: manager, answers: answers });
+  PubSub.subscribe(ANSWER, (type, payload) => answers.update(payload));
 }
 
 let types = {
@@ -16,19 +17,16 @@ var { ANSWER, CORRECT, WRONG } = types;
 var answers = {
   [CORRECT]: 0,
   [WRONG]: 0,
-};
-
-let fx = {
-  updateResult({correct, bonus}) {
+  update: function update({ correct, bonus}) {
     if (correct) {
-      answers[CORRECT]++;
-      if (bonus){
-        answers[CORRECT]++;
+      this[CORRECT]++;
+      if (bonus) {
+        this[CORRECT]++;
       }
     }
     if (!correct) {
-      answers[WRONG]++;
-    }
+      this[WRONG]++;
+    }    
   }
 };
 
@@ -51,8 +49,17 @@ class SlideManager {
     
     return this;
   }
+  
+  get length() {
+    return this.slides.length;
+  }
+  
+  isEmpty() {
+    return this.length === 0;
+  }
+  
   start() {
-    if (this.current < 0) {
+    if (!this.isEmpty() && this.current < 0) {
       this.current = 0;
     }
     this.$emit('start', [this.current]);
@@ -62,13 +69,56 @@ class SlideManager {
 
   move(index) {
     if (this.isMoveAllowed(index)) {
-      index = index % this.slides.length;
+      index = index % this.length;
 
       var oldIndex = this.current;
       this.current = index;
       this.$emit('move', [index, oldIndex]);      
     }
     return this;
+  }
+
+  isMoveAllowed(index) {
+    return (this.length > 0 && index >= 0 && index < this.length);
+  }
+  
+  $emit(...args){
+    this.pubsub.publish(...args);
+  }
+  
+  $on(...args) {
+    this.pubsub.subscribe(...args);
+  }
+}
+
+SlideManager.prototype.defaults = {
+  slides: [],
+  current: -1,
+  oninit: null,
+  onstart: null,
+  onmove: null,
+}
+
+class MySlideManager extends SlideManager {
+  constructor() {
+    super();
+    var slider = this;
+    slider.init({
+      slides: data.slides,
+      onstart: function onstart(event, currentIndex) {
+        slider.move(currentIndex);
+      },
+      onmove: function onmove(event) {
+        slider.isShowAll = false;
+      },
+      onshowall: function onshowall(event) {
+        slider.toggle('isShowAll');
+      },
+      onshowresults: function onshowresults(event) {
+        slider.toggle('isShowResults');
+      }
+    })
+    .start();
   }
   next() {
     return this.move(this.current + 1);
@@ -78,83 +128,25 @@ class SlideManager {
   }
   showAll() {
     this.$emit('showall');
-    return this;
   }
-  
-  isMoveAllowed(index) {
-    if (this.slides.length === 0) {
-      return false;
-    }
-    if (this.enableLoop) {
-      return true;
-    }
-    if (index >= 0 && index < this.slides.length) {
-      return true;
-    }
-    
-    return false;
-  }
-  
-  $emit(...args){
-    this.pubsub.publish(...args);
-    return this;
-  }
-  $on(...args) {
-    this.pubsub.subscribe(...args);
-    return this;
-  }
-}
-
-SlideManager.prototype.defaults = {
-  slides: [],
-  current: -1,
-  isShowAll: false,
-  enableLoop: false,
-  oninit: null,
-  onstart: null,
-  onmove: null,
-  onshowall: null
-}
-
-class MySlideManager extends SlideManager {
-  constructor() {
-    super();
-    
-    var slider = this;
-    slider.init({
-      slides: data.slides,
-      enableLoop: false,
-      oninit: function oninit() {
-      },
-      onstart: function onstart(event, currentIndex) {
-        slider.move(currentIndex);
-      },
-      onmove: function onmove(event) {
-        slider.isShowAll = false;
-      },
-      onshowall: function onshowall(event) {
-        let { move, current, slides } = slider;
-        if (slider.isShowAll) {
-          move(current);
-        } else {
-          slider.isShowAll = true;
-        }
-      },
-
-      isShowResults: false,
-      onshowresults: function onshowresults(event) {
-        slider.isShowResults = !slider.isShowResults;
-      }
-    });
-    
-    return this;
-  }
-  
   showResults() {
     this.$emit('showresults');
-    return this;
   }
-}
+  toggle(prop) {
+    this[prop] = !this[prop];
+  }
+} 
+
+MySlideManager.prototype.defaults = Object.assign(
+  {},
+  MySlideManager.prototype.defaults, 
+  {
+    isShowAll: false,
+    isShowResults: false,
+    onshowall: null,
+    onshowresults: null
+  }
+);
 
 // bind methods
 boundClass(SlideManager);
@@ -165,17 +157,14 @@ function initVue({manager, answers}){
   Vue.component('x-slide', {
     template: '#x-slide',
     props: ['title', 'questions'],
-    data: function() { return {}; }
   });
   Vue.component('x-slide-header', {
     template: '#x-slide-header',
     props: ['title'],
-    data: function() { return {}; },
   });
   Vue.component('x-slide-body', {
     template: '#x-slide-body',
     props: ['questions'],
-    data: function() { return {}; },
   });
   Vue.component('x-slide-question', {
     template: '#x-slide-question',
@@ -190,21 +179,25 @@ function initVue({manager, answers}){
     methods: {
       answered(correct) {
         if (correct) {
-          this.handled = true;
+          this.handle();
         }
         this.answerCount += 1;
         PubSub.publish(ANSWER, { correct: correct, bonus: this.bonus});
+      },
+      handle() {
+        this.handled = true;
       }
     },
     watch: {
-      answerCount(value, oldVal) {
-        if (value == this.answers.length-1 && !this.handled) {
-          this.handled = true;
+      answerCount(currentCount, oldCount) {
+        if (!this.handled && currentCount === this.answers.length-1) {
+          this.handle();
           this.revealAnswer = true;
         }
       }
     }
   });
+  
   Vue.component('x-slide-answer', {
     template: '#x-slide-answer',
     props: ['text', 'correct', 'handled', 'revealed'],
@@ -228,7 +221,6 @@ function initVue({manager, answers}){
   });
   Vue.component('x-slide-bonus', {
     template: '#x-slide-bonus',
-    data: function() { return {}; },
   });  
   
   var App = new Vue({
@@ -241,37 +233,49 @@ function initVue({manager, answers}){
       }
     },
     methods: {
-      isShow(index) {
-        return index == this.manager.current || this.manager.isShowAll;
+      show(index) {
+        return manager.isShowAll || index === manager.current;
       }
     }
   });    
   
-  var Footer = new Vue({
+  var Total = new Vue({
     el: '#total',
     data: function() {
       return {
         answers: answers,
-        manager: manager
+        manager: manager,
       }
     },
     computed: {
-      correct: function(){ return this.answers.correct; },
-      wrong: function(){ return this.answers.wrong; }
+      showResults: () => manager.isShowResults,
+      correct: () => answers.correct,
+      wrong: () => answers.wrong
     }
   });
   
   var Buttons = new Vue({
     el: '#buttons',
-    data: function() { return { }; },
     methods: {
-      next: () => manager.next(),
-      prev: () => manager.prev(),
-      showAll: () => manager.showAll(),
-      showResults: () => manager.showResults()
+      next: manager.next,
+      prev: manager.prev,
+      showAll: manager.showAll,
+      showResults: manager.showResults
     }
   });
  
+}
+
+const translations = {
+  buttons: {
+    next: 'הבא',
+    previous: 'הקודם',
+    showall: 'הצג הכל',
+    total: 'סיכום',
+  }
+}
+function translate(path) {
+  return _.get(translations, path, path);
 }
 
 main();
